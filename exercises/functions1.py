a137e1cfb68c8b2f463380308a88d9d8def3c009
ilsedelangen <ilsedelangen@hotmail.com>
2024-03-21 08:47:53 +0100

    Add solution function1.py

--- exercises/functions1.py
+++ exercises/functions1.py
@@ -19,3 +19,28 @@ class FunctionExample1:
                 paths.append(self.dir_path(interim))
                 interim += one_hour
         return paths
+    
+####################### SOLUTION ############################    
+
+class FunctionExample_solution:
+    """ Did not change much except some names"""
+    
+    def dir_path(self, start: datetime) -> str:
+        # Dummy Function for demonstration purposes
+        return start.strftime('%Y_%M_%D_%H')
+
+    def append_data_paths(self, start: datetime, stop: datetime = None) -> list:
+        paths = [self.dir_path(start)]
+        if stop is not None:
+            one_hour = timedelta(hours=1)
+            current_time = start + one_hour    
+            while current_time.hour <= stop.hour or current_time.day < stop.day:
+                paths.append(self.dir_path(current_time))
+                current_time += one_hour
+        return paths
+    
+    
+        
+            
+    
+    

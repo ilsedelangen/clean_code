a59b1329460acdaa8c667f42021079bdf8b4dc45
ilsedelangen <ilsedelangen@hotmail.com>
2024-03-21 13:23:08 +0100

    Add solutions functions3

--- exercises/functions3.py
+++ exercises/functions3.py
@@ -2,6 +2,7 @@ import os
 import csv
 import numpy as np
 from scipy import interpolate
+import matplotlib.pyplot as plt
 
 
 # Original version from the IOSP example
@@ -17,3 +18,34 @@ def input_flux(wl):
     flux = flux[1:, :]
     tck = interpolate.splrep(flux[:, 0], flux[:, 1], s=0)
     return np.array(interpolate.splev(wl, tck))
+
+################################# Solution ####################################
+
+def load_flux_from_csv(filename):
+    flux = np.genfromtxt(filename, delimiter=',', dtype=float, skip_header=1)
+    return flux 
+
+def interpolate_flux(wavelength,flux):
+    tck = interpolate.splrep(flux[:, 0], flux[:, 1], s=0)
+    return np.array(interpolate.splev(wavelength, tck))   
+
+def determine_input_flux(wavelength):
+    filename = os.path.join(os.path.dirname(__file__), '..', 'resources', 'flux.csv')
+    raw_flux = load_flux_from_csv(filename)
+    return interpolate_flux(wavelength,raw_flux) 
+
+# Not correct yet! 
+wl = np.arange(0,100)
+original = input_flux(wl)
+solution = determine_input_flux(wl)
+plt.plot(wl,original)
+plt.plot(wl,solution)
+plt.show()
+
+
+    
+
+
+
+
+

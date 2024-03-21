cd75bb17e31bda1007bd303ff1b4e59acd94d27a
Johannes Hölken <hoelken@mps.mpg.de>
2024-03-21 17:18:40 +0100

    Exam/project preparation

---
+++ exam/hangman.py
@@ -0,0 +1,11 @@
+"""
+Entry point for the hangman game.
+
+Run the game from the command line with
+    python -m exam.hangman
+
+Depending on your installation, you might need
+to call python3 instead of python.
+"""
+
+print("Hello World!")

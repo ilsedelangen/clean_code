a97fcb520e1fffa78155ac1d668e1604f7d8d543
ilsedelangen <ilsedelangen@hotmail.com>
2024-03-22 15:29:19 +0100

    Implemented hangman up and until winning of the game

--- exam/hangman.py
+++ exam/hangman.py
@@ -8,4 +8,86 @@ Depending on your installation, you might need
 to call python3 instead of python.
 """
 
-print("Hello World!")
+import numpy as np
+import os
+import random
+
+class Hangman():
+    
+    def __init__(self, max_attempts = 12):
+        current_folder = os.path.dirname(__file__)
+        self.path_to_file = os.path.join(current_folder, '../resources/words.txt')
+        self.database = self.load_words()
+        self.random_word = self.select_random_word()
+        self.current_word = '_'*len(self.random_word)
+        self.correct_characters = set()
+        self.wrong_characters = set()
+        self.max_attempts = max_attempts
+   
+    def load_words(self):
+        with open(self.path_to_file, 'r') as words_file:
+            words_database = words_file.read().splitlines()
+        return words_database        
+    
+    def select_random_word(self):
+        return random.choice(self.database)
+    
+    def ask_user_for_character(self):
+        input_character = input('Guess a character:')
+        return input_character.lower()
+    
+    def check_character(self, character):
+        if character in self.random_word:
+            self.correct_characters.add(character)
+        else:
+            self.wrong_characters.add(character)
+            if len(self.wrong_characters) >= self.max_attempts:
+                self.end_game()               
+                
+    def update_word(self):
+        updated_word = ''
+        for character in self.random_word:
+            if character in self.correct_characters:
+                updated_word += character
+            else:
+                updated_word += '_'
+        self.current_word = updated_word
+        
+    def report_progress(self):
+        print('Your word:', self.current_word, 'Used (wrong) characters:', list(self.wrong_characters) )
+      
+    def make_new_guess(self):
+        input_character = self.ask_user_for_character()
+        self.check_character(input_character)
+        self.update_word()
+        self.report_progress()
+        self.check_if_won()
+        
+    # def play_game(self, input):
+    #     while self.current_word != self.random_word:
+    #         self.make_new_guess(input)
+    #     self.win_game()
+    
+    def check_if_won(self):
+        if self.current_word == self.random_word:
+            self.win_game()
+        
+    def end_game(self):
+        print('Game over!')
+        
+    def win_game(self):
+        print('Congratulations!')
+    
+    
+
+# game = Hangman()    
+# game.random_word = 'ivy'
+# game.make_new_guess('i')
+# game.make_new_guess('v')
+# game.make_new_guess('y')
+# game.make_new_guess('z')
+
+
+
+
+
---
+++ tests/exam/hangman_test.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 22 12:56:37 2024
+
+@author: ilsed
+"""
+from ...exam.hangman import Hangman
+
+def test_ask_input(mocker):
+    game = Hangman()
+    mocker.patch('builtins.input', return_value ='F')
+    assert game.ask_user_for_character() == 'f'
+
+def test_load_words():
+    game = Hangman()
+    assert game.load_words()[0] == "abruptly"
+    assert game.load_words()[4] == "askew"
+    
+def test_select_word():
+    game = Hangman()
+    assert game.select_random_word() in game.database
+ 
+def test_check_character():
+    game = Hangman()
+    game.random_word = 'buffoon'
+    game.check_character('b')
+    game.check_character('o')
+    game.check_character('z')
+    assert game.correct_characters == {'b','o'}
+    assert game.wrong_characters == {'z'}
+        
+def test_update_word(mocker):
+    # mocker.patch('...exam.hangman.select_random_word', return_value ='buffoon')
+    game = Hangman()  
+    game.random_word = 'buffoon'
+    game.check_character('f')
+    game.check_character('b')
+    game.check_character('z')
+    game.update_word()
+    assert game.current_word == 'b_ff___'
+    
+def test_report_progress(mocker):
+    printer = mocker.patch('builtins.print')
+    game = Hangman()  
+    game.random_word = 'buffoon'
+    game.check_character('f')
+    game.update_word()
+    game.report_progress()
+    printer.assert_called_with('Your word:','__ff___','Used (wrong) characters:', [])
+    
+def test_max_attempts(mocker):
+    printer = mocker.patch('builtins.print')
+    game = Hangman(max_attempts=2)  
+    game.random_word = 'buffoon'
+    game.check_character('a')
+    game.check_character('c')
+    game.check_character('a')
+    game.check_character('z')
+    printer.assert_called_with('Game over!')
+
+# def test_win_game(mocker):
+#     game = Hangman()
+#     mocker.patch('builtins.input', return_value ='i')
+#     game.make_new_guess()
+#     mocker.patch('builtins.input', return_value ='v')
+#     game.make_new_guess()
+#     mocker.patch('builtins.input', return_value ='y')
+#     game.make_new_guess()
+#     assert game.current_word == 'ivy'
+    
+def test_win_game2(mocker): 
+    printer = mocker.patch('builtins.print')
+    game = Hangman()
+    game.random_word = 'ivy'
+    game.check_character('i')
+    game.update_word()
+    game.check_if_won()
+    game.check_character('v')
+    game.update_word()
+    game.check_if_won()
+    game.check_character('y')
+    game.update_word()
+    game.check_if_won()
+    printer.assert_called_with('Congratulations!')
+    
+
+
+    
+    
